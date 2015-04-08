3e70128f868127908899cbf430ab2224d6ab3742
iantpryor <iantpryor@gmail.com>
2015-04-08 12:07:27 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function(window, document, undefined) { 
+    window.onload = init;
+    function init() {
+        //get the canvas
+        var canvas = document.getElementById("mapcanvas");
+        var c = canvas.getContext("2d");
+        var width = 960;
+        var height = 540;
+    }
+})(window, document, undefined);
