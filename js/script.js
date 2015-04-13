63f82cafdf4524a53aee562754b306bb0fb951f0
iantpryor <iantpryor@gmail.com>
2015-04-13 12:11:29 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,15 @@
         var width = 960;
         var height = 540;
     }
+    
+    document.getElementById("paintbtn").onclick = paint;
+    
+    function paint(){
+        var widthSelect = document.getElementById("width");
+        width = widthSelect.options[widthSelect.selectedIndex].value;
+        
+        var heightSelect = document.getElementById("height");
+        height = heightSelect.options[heightSelect.selectedIndex].value;
+        
+    }
 })(window, document, undefined);
