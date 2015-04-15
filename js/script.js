d444709772331bc1b017865db3899cfd0bccfbe8
iantpryor <iantpryor@gmail.com>
2015-04-15 12:20:28 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -188,7 +188,7 @@
                
                for(var l = 0; l< 30; l++){
                    c.fillStyle = "#FFFFFF";
-                   c.fillRect(movingpointold.x*30 + l*movedirX + 10, movingpointold.y*30 + l*movedirY + 10, 6, 6);
+                   c.fillRect(movingpointold.x*30 + l*movedirX + 8, movingpointold.y*30 + l*movedirY + 8, 6, 6);
                }
                
                c.fillStyle = "#FFFFFF";
