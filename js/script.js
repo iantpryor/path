d0c0ee42de2aa02fa053812f9446f9c8ce4e7187
iantpryor <iantpryor@gmail.com>
2015-04-15 16:01:52 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@
             n = parseInt(n,10);
             if (isNaN(n)) return "00";
             n = Math.max(0,Math.min(n,255));
-            return "0123456789ABCDEF".charAt((n-n%16)/16) + "0123456789ABCDEF".charAt(n%16);
+            return "#" + "0123456789ABCDEF".charAt((n-n%16)/16) + "0123456789ABCDEF".charAt(n%16);
         }
         
         function floodfill(node, targetcolor, replacementcolor){
