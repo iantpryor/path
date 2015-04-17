047319959a23bc03979dba2b111474a49f44cd9a
iantpryor <iantpryor@gmail.com>
2015-04-17 11:47:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -322,6 +322,14 @@
                }
            }
            
+           //paint the original rooms
+           for(var i = 0; i< origroomlist.length; i++){
+                c.beginPath();
+                c.fillStyle = "#33CCFF";
+                c.fillRect(origroomlist[i].x*30 + 6, origroomlist[i].y*30 + 6, origroomlist[i].width*30 - 6*2, origroomlist[i].height*30 - 6*2);
+                c.stroke();
+            }
+           
            //paint the rooms so they look nice
            for(var i = 0; i< roomlist.length; i++){
                 c.beginPath();
@@ -329,6 +337,8 @@
                 c.fillRect(roomlist[i].x*30 + 6, roomlist[i].y*30 + 6, roomlist[i].width*30 - 6*2, roomlist[i].height*30 - 6*2);
                 c.stroke();
             }
+            
+            
             
             //paint the start, branch, and end points.
             c.fillStyle = "#33CC33";
